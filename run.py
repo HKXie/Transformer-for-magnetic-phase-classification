f14c16044f389dbd03e91702f7757d55ef7f37de
GitHub 用户名 <2429295697@qq.com>
2024-05-27 22:04:49 +0800

    modified

--- run.py
+++ run.py
@@ -8,7 +8,7 @@ from einops import rearrange
 # from torchvision import datasets, transforms
 from torch.utils.data import Dataset, SubsetRandomSampler, DataLoader
 
-import argparse
+import configargparse
 
 import numpy as np
 import matplotlib.pylab as plt
@@ -18,8 +18,10 @@ import train
 
 if __name__ == '__main__':
 
-    parser = argparse.ArgumentParser(description='Vit_heisenberg_pahses_classification')
+    parser = configargparse.ArgumentParser(description='Vit_heisenberg_pahses_classification')
 
+    parser.add_argument('--config', is_config_file=True, default='configs.txt',
+                        help='config file path')
     parser.add_argument('--data_path', type=str, default='data',help='')
     parser.add_argument('--train_data_name', type=str,
                         default=['124_MT_20size_1.5T_jiangede.hdf5',],help='')
@@ -27,15 +29,15 @@ if __name__ == '__main__':
     parser.add_argument('--test_data_name', type=str,
                         default=['124_MT_20size_2T_jiangede.hdf5',],help='')
 
-    parser.add_argument('--batch-size', type=int, default=128, metavar='N', help='input batch size for training (default: 128)')
+    parser.add_argument('--batch_size', type=int, default=128, metavar='N', help='input batch size for training (default: 128)')
     parser.add_argument('--epochs', type=int, default=100, metavar='N',help='number of epochs to train (default: 100)')
     parser.add_argument('--learning_rate', type=float, default=5e-5)
-    parser.add_argument('--no-cuda', action='store_true', default=False, help='enables CUDA training')
-    parser.add_argument('--imgage-size', type=int, default=20,help='input image size')
-    parser.add_argument('--imgage-patch', type=int, default=4,help='segmentation image patch')
+    parser.add_argument('--no_cuda', action='store_true', default=False, help='enables CUDA training')
+    parser.add_argument('--imgage_size', type=int, default=20,help='input image size')
+    parser.add_argument('--imgage_patch', type=int, default=4,help='segmentation image patch')
     parser.add_argument('--channels', type=int, default=2, help='_')
-    parser.add_argument('--num-class', type=int, default=2,help='num class')
-    parser.add_argument('--mlp-dim', type=int, default=128, help='_')
+    parser.add_argument('--num_class', type=int, default=2,help='num class')
+    parser.add_argument('--mlp_dim', type=int, default=128, help='_')
     parser.add_argument('--num_token',type=int, default=96, help='_')
     parser.add_argument('--dropout-rate', type=int, default=0.3, help='_')
 
@@ -53,6 +55,3 @@ if __name__ == '__main__':
 
     exp = train.Vit_train_task(Model_vit, args)
     exp.train_valid()
-
-
-
